60388e2833c7abeb327c1a8dd1d07a6e7736e1e8
JongKyuHong <65146977+JongKyuHong@users.noreply.github.com>
2020-10-09 19:38:22 +0900

    20201009

---
+++ 알고리즘/201009/상하좌우.py
@@ -0,0 +1,21 @@
+n = int(input())
+direction = list(input().split(' '))
+location = [1,1]
+
+for i in direction:
+    if i == 'R':
+        if location[1] != n:
+            location[1] += 1
+    elif i == 'L':
+        if location[1] != 1:
+            location[1] -= 1
+    elif i == 'U':
+        if location[0] != 1:
+            location[0] -= 1
+    elif i == 'D':
+        if location[0] != n:
+            location[0] += 1
+print(location[0],location[1])
+
+
+
