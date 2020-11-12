fb11a1db1ef6066b597eb9f9cdb7de129b53b7ff
JongKyuHong <65146977+JongKyuHong@users.noreply.github.com>
2020-11-12 23:33:13 +0900

    20201112

---
+++ 알고리즘/20201112/사이클 판별 소스.py
@@ -0,0 +1,28 @@
+def find_parent(parent,x):
+    pass
+
+def union_parent(parent,a,b):
+    pass
+
+v,e = map(int,input().split())
+parent = [0] * (v+1)
+
+for i in range(1,v+1):
+    parent[i] = i
+
+cycle = False
+
+for i in range(e):
+    a,b = map(int,input().split())
+    if find_parent(parent,a) == find_parent(parent,b):
+        cycle = True
+        break
+    else:
+        union_parent(parent,a,b)
+
+if cycle:
+    print("사이클이 발생했습니다.")
+else:
+    print("사이클이 발생하지 않았습니다.")
+
+
