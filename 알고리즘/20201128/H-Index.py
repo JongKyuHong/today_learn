128dac060017c99533704137544de8b3aecfff7b
JongKyuHong <65146977+JongKyuHong@users.noreply.github.com>
2020-11-28 01:58:52 +0900

    20201128

---
+++ 알고리즘/20201128/H-Index.py
@@ -0,0 +1,5 @@
+def solution(citations):
+    for i in range(len(citations)):
+        if citations[i] 
+    answer = 0
+    return answer
---
+++ 알고리즘/20201128/k번째 수.py
@@ -0,0 +1,14 @@
+def solution(array, commands):
+    answer = []
+    for i in range(len(commands)):
+        a = commands[i][0]
+        b = commands[i][1]
+        c = commands[i][2]
+        array2 = array[a-1:b]
+        array2.sort()
+        answer.append(array2[c-1])
+
+    return answer
+
+print(solution([1,5,2,6,3,7,4],[[2,5,3],[4,4,1],[1,7,3]]))
+
