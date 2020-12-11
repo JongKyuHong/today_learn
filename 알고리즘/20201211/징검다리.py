f3f024a0478a176658310d3f966f1975884ce1c5
JongKyuHong <65146977+JongKyuHong@users.noreply.github.com>
2020-12-11 22:05:23 +0900

    20201211

--- 알고리즘/20201211/징검다리.py
+++ 알고리즘/20201211/징검다리.py
@@ -1,13 +1,13 @@
-def a(n,rocks):
-    
+from itertools import permutations
+
 
 def solution(distance, rocks, n):
     answer = 0
     start = 1
     end = distance
-
-    while start<=end:
-        mid = (start+end)//2
+    
 
 
     return answer
+
+print(solution(25,[2,14,11,21,17],2))
