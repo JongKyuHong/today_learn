0c78efd05772a1be72d991db1e4d1d05d4b9f604
JongKyuHong <65146977+JongKyuHong@users.noreply.github.com>
2020-12-11 14:43:09 +0900

    20201211

--- 알고리즘/20201211/소수 찾기.py
+++ 알고리즘/20201211/소수 찾기.py
@@ -1,5 +1,28 @@
+from itertools import permutations
+import math
+
+def check(number):
+    k = math.sqrt(number)
+
+    if number < 2:
+        return False
+
+    for i in range(2,int(k)+1):
+        if number % i == 0:
+            return False
+    return True
+
+
 def solution(numbers):
-    answer = 0
+    answer = []
+    for i in range(1,len(numbers)+1):
+        perlist = list(map(''.join,permutations(list(numbers),i)))
+        print(perlist)
+        for k in list(set(perlist)):
+            if check(int(k)):
+                answer.append(int(k))
+    answer = len(set(answer))
     return answer
+
 
 
