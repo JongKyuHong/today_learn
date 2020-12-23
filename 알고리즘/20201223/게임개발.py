b7f8ccf790eea271cd73c51dfb8c05614c1b5164
JongKyuHong <65146977+JongKyuHong@users.noreply.github.com>
2020-12-23 20:37:20 +0900

    20201223

---
+++ 알고리즘/20201223/게임개발.py
@@ -0,0 +1,9 @@
+n,m = list(map(int,input().split()))
+a,b,d = list(map(int,input().split()))
+
+data = []
+for i in range(m):
+    data.append(list(map,input().split()))
+
+
+
--- 알고리즘/20201223/시각.py
+++ 알고리즘/20201223/시각.py
@@ -1,3 +1,8 @@
 n = int(input())
-
-for i in 
+answer = 0
+for i in range(n+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                answer += 1
+print(answer)
