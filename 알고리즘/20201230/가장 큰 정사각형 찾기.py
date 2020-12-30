c8c32cf128ca232413e25096c4e295851e0e100f
JongKyuHong <65146977+JongKyuHong@users.noreply.github.com>
2020-12-30 23:02:12 +0900

    20201230

--- 알고리즘/20201230/가장 큰 정사각형 찾기.py
+++ 알고리즘/20201230/가장 큰 정사각형 찾기.py
@@ -1,14 +1,29 @@
 def solution(board):
-    answer = 1234
-    #board_new = board.deepcopy()
-    def check(x,y,board):
-        for i in range(len(board)):
-            for j in range(len(board)):
+    answer = 1
+
+    N = len(board)
+    def check(x,y,n):
+        cnt = 1
+        for i in range(x,n):
+            for j in range(y,n):
                 if board[i][j] == 1:
                     if j+1 <= len(board) and i+1 <= len(board):
-                        check(i,j+1)
-                        check(i+1,j+1)
+                        a = check(i,j+1,n)
+                        b = check(i+1,j+1,n)
+                        c = check(i+1,j,n)
+                        if (a == True and b == True) and c == True:
+                            nonlocal answer
+                            nonlocal cnt
+                            cnt += 1
+                            if cnt >= answer:
+                                answer = cnt
+                    else:
+                        return False
+                else:
+                    return False
+        return True
 
-    return answer
+    check(0,0,N)
+    return answer**2
 
 print(solution([[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]))
