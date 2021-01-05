891135257fe810c488b3fb7220b142fd746488c2
JongKyuHong <65146977+JongKyuHong@users.noreply.github.com>
2021-01-05 21:21:04 +0900

    20210105

---
+++ 알고리즘/20210105/배수와 약수.py
@@ -0,0 +1,15 @@
+while 1:
+    n,m = map(int,input().split())
+    if n == 0 and m == 0:
+        break
+    if n > m:
+        if n % m == 0:
+            print("multiple")
+        else:
+            print("neither")
+    else:
+        if m % n == 0:
+            print("factor")
+        else:
+            print("neither")
+
---
+++ 알고리즘/20210105/약수.py
@@ -0,0 +1,10 @@
+n = int(input())
+m = list(map(int,input().split()))
+result = m[0] * m[-1]
+
+
+print(result)
+
+
+
+
