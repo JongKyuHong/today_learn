ffc32d0ca88c5ea17b2fde5a9b99b5a133d81302
JongKyuHong <65146977+JongKyuHong@users.noreply.github.com>
2021-01-12 21:29:47 +0900

    20210112

--- 알고리즘/20210112/계단 오르기(re).py
+++ 알고리즘/20210112/계단 오르기(re).py
@@ -1,12 +1,23 @@
-n = int(input())
-stair = []
-dp = [[] for i in range(n)]
-for _ in range(n):
+import sys
+input = sys.stdin.readline
 
-for i in range(n):
-    stair.append(int(input()))
-    if i == 0:
-        dp[0].append(stair[0])
-    dp[i].append(stair+)
+def solve(stair,n):
+    dp = []
+    dp.append(stair[0])
+    for i in range(1,3):
+        if i == 1:
+            dp.append(max(dp[i-1]+stair[i],stair[i]))
+            continue
+        dp.append(max(dp[i-2]+stair[i],stair[i-1]+stair[i]))
+    for i in range(3,n):
+        dp.append(max(stair[i] + stair[i-1] + dp[i-3],stair[i]+dp[i-2]))
+    print(dp[-1])
 
-
+if __name__ == '__main__':
+    stair = []
+    n = int(input().strip())
+    for i in range(n):
+        stair.append(int(input().strip()))
+    solve(stair,n)
+    
+# https://daimhada.tistory.com/181
