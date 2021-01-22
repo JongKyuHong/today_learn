e8cf13559277fa9d0ca9b1a618e9b5489c42a0b8
JongKyuHong <65146977+JongKyuHong@users.noreply.github.com>
2021-01-22 19:12:07 +0900

    20210122

---
+++ 알고리즘/20210121/csp.py
@@ -0,0 +1,49 @@
+from typing import Generic, TypeVar, Dict, List, Optional
+from abc import ABC,abstractmethod
+
+V = TypeVar('V')
+D = TypeVar('D')
+
+class Constraint(Generic[V,D],ABC):
+    def __init__(self,variables: List[V]) ->None:
+        self.variables = variables
+
+    @abstractmethod
+    def satisfied(self,assignment: Dict[V,D]) -> bool:
+        ...
+
+class CSP(Generic[V,D]):
+    def __init__(self,variables: List[V], domains: Dict[V,List[D]]) -> None:
+        self.variables: List[V] = variables
+        self.domains: Dict[V,List[D]]
+        self.constraints:Dict[V,List[Constraint[V,D]]] = {}
+        for variable in self.variables:
+            self.constraints[variable] = []
+            if variable not in self.domains:
+                raise LookupError("모든 변수에 도메인이 할당되어야 합니다.")
+    def add_constraint(self, constraint: Constraint[V,D]) -> None:
+        for variable in constraint.variables:
+            if variable not in self.variables:
+                raise LookupError("제약 조건 변수가 아닙니다.")
+            else:
+                self.constarints[variable].append(constraint)
+    def consistent(self, variable:V, assignment: Dict[V,D]) ->bool:
+        for constarint in self.constarints[variable]:
+            if not constarint.satisfied(assignment):
+                return False
+        return True
+    def backtracking_search(self, assignment: Dict[V,D] = {}) -> Optional[Dict[V,D]]:
+        if len(assignment) == len(self.variables):
+            return assignment
+        unassigned: List[V] = [v for v in self.variables if v not in assignment]
+        
+        first: V = unassigned[0]
+        for value in self.domains[first]:
+            local_assignment = assignment.copy()
+            local_assignment[first] = value
+            if self.consistent(first,local_assignment):
+                result: Optional[Dict[V,D]] = self.backtracking_search(local_assignment)
+                if result is not None:
+                    return result
+        return None
+
