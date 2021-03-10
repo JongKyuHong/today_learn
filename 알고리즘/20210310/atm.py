c6c15616ff4a83288b75127cbf56f150cea024de
JongKyuHong <65146977+JongKyuHong@users.noreply.github.com>
2021-03-10 19:59:13 +0900

    20210310

--- 알고리즘/20210310/atm.py
+++ 알고리즘/20210310/atm.py
@@ -1,5 +1,13 @@
 n = int(input())
 a = list(map(int,input().split()))
-
-
+if n == 1:
+    print(a[0])
+else:
+    a.sort()
+    result = 0
+    q = 0
+    for i in range(n):
+        result += q+a[i]
+        q += a[i]
+    print(result)
 
