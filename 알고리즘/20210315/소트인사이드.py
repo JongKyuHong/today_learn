2e266a82d0ed5fc6ffe00a4212ea245ee1736509
JongKyuHong <65146977+JongKyuHong@users.noreply.github.com>
2021-03-16 03:55:10 +0900

    20210316

---
+++ 알고리즘/20210315/소트인사이드.py
@@ -0,0 +1,10 @@
+n = input()
+s = []
+for i in n:
+    s.append(i)
+s.sort(reverse=True)
+print(''.join(s))
+
+
+
+
---
+++ 알고리즘/20210315/수 정렬하기2.py
@@ -0,0 +1,7 @@
+n = int(input())
+s = []
+for _ in range(n):
+    s.append(int(input()))
+s.sort()
+for i in s:
+    print(i)
